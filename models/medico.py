7669f0737962b4baa3b9516bb6aee5f9f2d83d95
IagoHvck <iagopokalmeida@gmail.com>
2024-08-06 14:14:05 -0300

    medico.py

---
+++ models/medico.py
@@ -0,0 +1,6 @@
+class Medico:
+    def __init__(self, id, nome, especialidade, crm):
+        self.id = id
+        self.nome = nome
+        self.especialidade = especialidade
+        self.crm = crm
