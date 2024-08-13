78fedf04f6b2673f86f190f624437d840204b40c
Janio-Almeida <janiovenoom@gmail.com>
2024-08-13 15:19:48 -0300

    adicionado controller/paciente, models/paciente, repositories/repository

---
+++ models/paciente.py
@@ -0,0 +1,7 @@
+class Paciente:
+    def __init__(self, id_paciente = None, cpf = None, nome = None, data_nascimento = None, telefone = None):
+        self.id_paciente = id_paciente
+        self.cpf = cpf
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.telefone = telefone
