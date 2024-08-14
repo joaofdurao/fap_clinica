1be7a9cfc3c4c6401ea6f49ff5b730ccce609711
IagoHvck <iagopokalmeida@gmail.com>
2024-08-14 14:48:09 -0300

    medico.py

--- models/medico.py
+++ models/medico.py
@@ -1,6 +1,6 @@
 class Medico:
-    def __init__(self, id = None, nome = None, especialidade = None, crm = None):
-        self.id = id
+    def __init__(self, medico_id = None, nome = None, especialidade = None, crm = None):
+        self.medico_id = medico_id
         self.nome = nome
         self.especialidade = especialidade
         self.crm = crm
